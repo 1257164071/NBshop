498bd4b5338f80b7e977f83b8fb290e292667fc6
NightDays <1257164071@qq.com>
2021-04-20 11:44:18 +0800

    分销背景更新

--- app/api/controller/wap/Team.php
+++ app/api/controller/wap/Team.php
@@ -23,12 +23,13 @@ class Team extends Base
         $user = \app\common\model\users\Users::find($user);
 
 
-        if ($user['poster'] == null){
+//        if ($user['poster'] == null){
+        if (true){
             $code = new QRcode();
-            $code_path =  $code->png(request()->domain().'/public/register?parent_id='.$user->id,false,7)        //生成二维码
-                ->background(270,200,'static/images/bg.jpg')
-                ->text("我是<".$user->nickname.">",40,['center',1020],'#999999')
-                ->text('微信扫描二维码或长按识别',30,['center',1100],'#999999')
+            $code_path =  $code->png(request()->domain().'/public/register?parent_id='.$user->id,false,2.8)        //生成二维码
+                ->background(110,298,'static/images/bg2.jpg')
+                ->text($user->nickname,30,[100,253], '#ea0606')
+                ->text('微信扫描二维码或长按识别',30,['center',1100],'#fffffff')
                 ->getPath();
             $user->poster = $code_path;
             $user->save();
